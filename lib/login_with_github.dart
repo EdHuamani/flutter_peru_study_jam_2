4f0c0c1f8a9c7479b90fad27affe3b961fb6cb5d
Edgar Huamani <ehuamani@unasolutions.com>
2018-09-29 10:44:58 -0500

    First commit

---
+++ lib/login_with_github.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginWithGithub extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4.0))),
+      textColor: Colors.white,
+      icon: SvgPicture.asset(
+        'assets/images/github_mark.svg',
+        color: Colors.white,
+      ),
+      onPressed: () {},
+      label: RichText(
+        text: TextSpan(children: [
+          TextSpan(text: 'Login with '),
+          TextSpan(
+              text: 'Github', style: TextStyle(fontWeight: FontWeight.bold))
+        ]),
+      ),
+      color: Color.fromRGBO(93, 179, 68, 1.0),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:study_jam_peru_2/login_with_github.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'FlutterHub'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(widget.title),
+        ),
+        body: new Center(
+          child: LoginWithGithub(),
+        ));
+  }
+}
